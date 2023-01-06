f814108e104117501eeb26179da6974dad63df82
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-05 17:34:55 -0800

    create Header component with standard styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Header/Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={styles["header"]}>
+            <div className={styles["container"]}></div>
+        </header>
+    )
+}
