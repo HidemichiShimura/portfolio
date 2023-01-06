4a72f730a4984fa32f0e58f237ac566c9969a503
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-06 02:07:22 -0800

    add <HeaderLogo /> to <Header />

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import styles from "../../styles/Header/Header.module.css";
 
+import { HeaderLogo } from "../index";
+
 export default function Header() {
     return (
         <header className={styles["header"]}>
-            <div className={styles["container"]}></div>
+            <div className={styles["container"]}>
+                <HeaderLogo />
+            </div>
         </header>
     )
 }
