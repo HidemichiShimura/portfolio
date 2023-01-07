51a30017aac88307bcc915df45b7e4cc68afd80f
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-06 16:43:13 -0800

    create BurgerMenu components

---
+++ src/components/Header/BurgerMenu/BurgerNav.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import styles from "../../../styles/Header/BurgerMenu/BurgerNav.module.css";
+
+export default function BurgerNav(props) {
+    const {isOpen} = props;
+
+    return (
+        <nav className={isOpen ? styles["open-nav"] : styles["nav"]}>
+            <div className={styles["inner"]}>
+                <ul>
+                    <li><a href="#intro">INTRO</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#projects">PROJECTS</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as HeaderLogo } from "./Header/HeaderLogo";
+export { default as BurgerMenu } from "./Header/BurgerMenu/BurgerMenu";
+export { default as BurgerNav } from "./Header/BurgerMenu/BurgerNav";
+export { default as ToggleBtn } from "./Header/BurgerMenu/ToggleBtn";
