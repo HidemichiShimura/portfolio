0375ce9401de20ce5bb4f5aec436f6e3a3dbd677
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-06 16:46:08 -0800

    embed BurgerMenu component to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import styles from "../../styles/Header/Header.module.css";
 
-import { HeaderLogo } from "../index";
+import { HeaderLogo, BurgerMenu } from "../index";
 
 export default function Header() {
     return (
         <header className={styles["header"]}>
             <div className={styles["container"]}>
                 <HeaderLogo />
+                <BurgerMenu />
             </div>
         </header>
     )
