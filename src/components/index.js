b5fb9e801697f398f023199b164d330b69deb8b7
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-12 17:32:41 -0800

    add the Main component, which contains Intro, About, Projects, and Contact sections

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,14 @@
+/*
+ * Header
+ */
 export { default as Header } from "./Header/Header";
 export { default as HeaderLogo } from "./Header/HeaderLogo";
 export { default as BurgerMenu } from "./Header/BurgerMenu/BurgerMenu";
 export { default as BurgerNav } from "./Header/BurgerMenu/BurgerNav";
 export { default as ToggleBtn } from "./Header/BurgerMenu/ToggleBtn";
 export { default as NavMenu } from "./Header/NavMenu";
+
+/*
+ * Main
+ */
+export { default as Main } from "./main/Main";
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const MainSection = styled.main`
+    width: 100%;
+
+    @media only screen and (min-width: 751px) {
+        padding: 0 10%;
+    }
+`;
+
+const Main = () => {
+    return (
+        <MainSection></MainSection>
+    )
+}
+
+export default Main;
