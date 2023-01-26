4a35964a4aec5e1242741d5ee9d2bd28e96bcb0a
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2023-01-26 10:14:19 -0800

    fix horizontal padding of the Main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 const MainSection = styled.main`
   width: 100%;
-  padding: 0 5%;
+  padding: 0 40px;
   margin-top: 70px;
 
   @media only screen and (min-width: 751px) {
