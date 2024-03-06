16e741ba570778d0d797eb7db1308b30a2699eaf
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-03-05 17:52:46 -0800

    dev(env): handle module path aliases in Jest

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,9 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "./src/$1",
+  },
 };
 
 export default createJestConfig(config);
