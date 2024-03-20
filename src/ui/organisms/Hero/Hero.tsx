0b4a34334cb82698bb1b8b6e362b797ff5e6a5dc
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-03-19 19:00:43 -0700

    dev(ui): add <Hero />

---
+++ src/ui/organisms/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import { RadialLinearContainer, HeroHeadings } from "@/ui";
+
+export default function Hero() {
+  return (
+    <RadialLinearContainer>
+      <div className="flex h-screen items-center">
+        <HeroHeadings />
+      </div>
+    </RadialLinearContainer>
+  );
+}
