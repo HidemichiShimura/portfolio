b084f537c38e02633d16c45e4c3165a925623786
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-03-19 19:01:27 -0700

    dev(ui): remove unnecessary classes

--- src/ui/atoms/RadialLinearContainer/RadialLinearContainer.tsx
+++ src/ui/atoms/RadialLinearContainer/RadialLinearContainer.tsx
@@ -4,7 +4,7 @@ export default function RadialLinearContainer({
   children: React.ReactNode;
 }) {
   return (
-    <section className="animate-radial-gradient bg-radial-gradient flex items-center justify-center bg-[radial-gradient(circle,#7952B3,transparent,transparent)] bg-[length:100px_100px] bg-no-repeat md:bg-[length:350px_350px]">
+    <section className="bg-radial-gradient animate-radial-gradient bg-[radial-gradient(circle,#7952B3,transparent,transparent)] bg-[length:100px_100px] bg-no-repeat md:bg-[length:350px_350px]">
       {children}
     </section>
   );
