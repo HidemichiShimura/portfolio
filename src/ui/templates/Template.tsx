1214f36d0e93d95949c63fa81c6b0595484bcd19
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-03-27 20:29:00 -0700

    bugfix: change the footer width to the screen size

--- src/ui/templates/Template.tsx
+++ src/ui/templates/Template.tsx
@@ -3,16 +3,19 @@ import { Grid } from "@/ui";
 export default function Template({
   hero,
   content,
+  footer,
 }: {
   hero: React.ReactNode;
   content: React.ReactNode;
+  footer: React.ReactNode;
 }) {
   return (
     <>
       <div className="flex justify-center">{hero}</div>
       <Grid>
-        <div className="px-4 md:px-20">{content}</div>
+        <div className="px-4 pb-44 md:px-20">{content}</div>
       </Grid>
+      {footer}
     </>
   );
 }
