69d3609650b414d683a40b39d4a753c60037c759
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-03-28 23:02:32 -0700

    bugfix: fix radial gradient bg cutoff

--- src/ui/organisms/Hero/Hero.tsx
+++ src/ui/organisms/Hero/Hero.tsx
@@ -2,10 +2,12 @@ import { RadialLinearContainer, HeroHeadings } from "@/ui";
 
 export default function Hero() {
   return (
-    <RadialLinearContainer>
-      <div className="flex h-screen w-full items-center justify-center">
-        <HeroHeadings />
-      </div>
-    </RadialLinearContainer>
+    <div className="grow">
+      <RadialLinearContainer>
+        <div className="flex h-screen w-full items-center justify-center self-stretch">
+          <HeroHeadings />
+        </div>
+      </RadialLinearContainer>
+    </div>
   );
 }
