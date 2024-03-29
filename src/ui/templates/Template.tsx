d0a6c0686341a9afd8bd413e26496606340c2e69
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-03-29 03:15:49 -0700

    feat: implement theme toggle

--- src/ui/templates/Template.tsx
+++ src/ui/templates/Template.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useContext } from "react";
 import { Grid } from "@/ui";
+import { ThemeContext } from "@/contexts";
 
 export default function Template({
   hero,
@@ -9,13 +13,17 @@ export default function Template({
   content: React.ReactNode;
   footer: React.ReactNode;
 }) {
+  const theme = useContext(ThemeContext);
+
   return (
-    <main className="bg-black">
-      <div className="flex justify-center">{hero}</div>
-      <Grid>
-        <div className="px-4 pb-44 md:px-20">{content}</div>
-      </Grid>
-      {footer}
-    </main>
+    <div className={theme === "dark" ? "dark" : ""}>
+      <main className="bg-black">
+        <div className="flex justify-center">{hero}</div>
+        <Grid>
+          <div className="px-4 pb-44 md:px-20">{content}</div>
+        </Grid>
+        {footer}
+      </main>
+    </div>
   );
 }
