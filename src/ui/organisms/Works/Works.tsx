1b6b476d24d7a98c0f97bdeba093bf043eb1fb0f
Hidemichi Shimura <hidemichi.shimura@gmail.com>
2024-04-15 18:39:37 -0700

    dev: test organism <Works />

--- src/ui/organisms/Works/Works.tsx
+++ src/ui/organisms/Works/Works.tsx
@@ -2,7 +2,10 @@ import { SectionHeading, Work } from "@/ui";
 
 export default function Works() {
   return (
-    <section className="flex min-h-screen flex-col justify-center gap-8">
+    <section
+      className="flex min-h-screen flex-col justify-center gap-8"
+      title="Works"
+    >
       <SectionHeading>WORKS</SectionHeading>
       <div className="flex flex-col justify-center gap-4">
         <Work name="pf" />
